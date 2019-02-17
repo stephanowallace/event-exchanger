16ee3b9d7023c01df20d7a495109038053c14738
dielduarte <magdiel.kesller@gmail.com>
2019-02-17 20:23:26 -0300

    feat(EventWatcher): add background animation when an event happens

--- src/components/EventWatcher/index.js
+++ src/components/EventWatcher/index.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import style from './style.module.css';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import style from "./style.module.css";
 
 const EventWatcher = ({ className, eventName }) => {
   const [numberOfListens, setNumberOfListens] = useState(0);
+  const [flashAnimateClassName, setFlashAnimateClassName] = useState("");
+  const REMOVE_CLASSNAME_TIME = 200;
 
   function eventHandler(e) {
     setNumberOfListens(numberOfListens + 1);
@@ -14,8 +16,18 @@ const EventWatcher = ({ className, eventName }) => {
     return document.removeEventListener(eventName, e => eventHandler(e));
   });
 
+  useEffect(() => {
+    if (!numberOfListens) return;
+
+    setFlashAnimateClassName(style.flashAnimation);
+
+    setTimeout(() => {
+      setFlashAnimateClassName("");
+    }, REMOVE_CLASSNAME_TIME);
+  }, [numberOfListens]);
+
   return (
-    <div className={`${style.container} ${className}`}>
+    <div className={`${style.container} ${flashAnimateClassName} ${className}`}>
       <p className={style.name}>{eventName}</p>
       <p className={style.counter}>Listened {numberOfListens} times</p>
     </div>
