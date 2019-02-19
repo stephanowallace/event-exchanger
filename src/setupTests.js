eec5c7045817f90fcd133ee8e13572f78dd19ba6
Stéphano Wallace <lastephano.w@Stephanos-MacBook-Pro.local>
2019-02-19 10:43:43 -0300

    add react-testing-livrary setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
 
 configure({ adapter: new Adapter() });
