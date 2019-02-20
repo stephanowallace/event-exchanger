0684bf8856aaf1005e4c51e252f7ebf2f8edaf20
dielduarte <magdiel.kesller@gmail.com>
2019-02-20 20:53:43 -0300

    exporting webcomponent

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class EventExchanger extends HTMLElement {
+  constructor() {
+    super();
+    console.log('constructor EventExchanger ...');
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  connectedCallback() {
+    console.log('connected EventExchanger ....');
+    ReactDOM.render(<App />, this.attachShadow({ mode: 'open' }));
+  }
+}
+
+customElements.define('event-exchanger', EventExchanger);
+
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
